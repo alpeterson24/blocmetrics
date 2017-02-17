b3ce8dea42dafe3111819696379abb3a1c7fdb10
Allen Peterson <alpeterson2424@yahoo.com>
2017-02-16 18:59:38 -0600

    add spec for reg app controller

--- spec/controllers/registered_applications_controller_spec.rb
+++ spec/controllers/registered_applications_controller_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
+require 'random_data'
 
 RSpec.describe RegisteredApplicationsController, type: :controller do
+
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, id: application.id
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "redirects to the index view" do
+      delete :destroy
+      expect(response).to redirect_to registered_applications_path
+    end
+  end
 
 end
